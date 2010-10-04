f7f681ce87355ced997ec95b7ff8b17442fcf4d8
Paul van der Walt <paul@denknerd.org>
2010-10-04 13:20:06 +0200

    Initial commit: sequential sieve and makefile. Tested and working up to N==100.

---
+++ Makefile
@@ -0,0 +1,10 @@
+#CFLAGS=-lm
+
+all: sieve
+
+sieve: sieve.o
+	gcc -lm sieve.o -o sieve
+
+sieve.o: sieve.c
+	gcc -c sieve.c
+
---
+++ sieve.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+enum bool { true, false };
+int nextPrime(int, enum bool*) ;
+void printPrimes(int, enum bool*) ;
+
+int main(int argc, char** argv)
+{
+    int N, i;
+    printf("Hi! Welcome to sequential sieve.\n");
+    printf("Enter N = ");
+    scanf("%d", &N);
+
+    // --- begin
+
+    enum bool *A = malloc(sizeof(enum bool)*N);
+
+    A[0] = false;
+    for(i = 1; i < N; i++)
+    {
+        A[i] = true;
+    }
+
+    int current = 2;
+
+    while (current <= sqrt((double)N))
+    {
+        for(i = current; i <= N/current; i++)
+        {
+            A[i*current] = false;
+        }
+        current = nextPrime(current, A);
+    }
+
+    printPrimes(N, A);
+}
+
+int nextPrime(int c, enum bool*A) 
+{
+    int next = c+1; 
+    while ( A[next] != true)
+        next++;
+
+    return next;
+}
+
+void printPrimes(int N, enum bool* A)
+{
+    int i;
+    int nPrimes=0;
+    for(i = 0; i < N; i++)
+        if(A[i] == true)
+        {
+            printf("prime: %d\n", i);
+            nPrimes++;
+        }
+
+    printf("So that's %d primes.\n", nPrimes); 
+}
